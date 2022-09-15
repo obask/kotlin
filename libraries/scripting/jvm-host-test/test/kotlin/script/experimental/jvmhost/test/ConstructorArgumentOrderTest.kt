f14a1b3c50b224dd8ef64f92878071f3bd7d5e2b
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-09-15 20:18:58 +0200

    fixup! Fix lowering of receiver access in IR scripting

--- libraries/scripting/jvm-host-test/test/kotlin/script/experimental/jvmhost/test/ConstructorArgumentOrderTest.kt
+++ libraries/scripting/jvm-host-test/test/kotlin/script/experimental/jvmhost/test/ConstructorArgumentOrderTest.kt
@@ -5,7 +5,6 @@
 
 package kotlin.script.experimental.jvmhost.test
 
-import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import org.junit.Test
 import kotlin.script.experimental.api.*
 import kotlin.test.assertTrue
@@ -37,38 +36,6 @@ class ConstructorArgumentsOrderTest {
     }
 
     @Test
-    fun testKt53947_ScriptWithImplicitReceiverAndCapturing() {
-        // Reproducing (a bit extended) scenario from KT-53947: without the fix, in the presence of the implicit receiver
-        // of the same type as the receiver in the `apply` function body, the lowering was incorrectly substituting
-        // the correct receiver with the accessor to the implicit one
-        val res = evalString<ScriptWithImplicitReceiver>(
-            """
-                import kotlin.script.experimental.jvmhost.test.ImplicitReceiverClass
-                
-                val x = "Ok"
-                
-                class C1 {
-                    val y = x + "."
-                }
-                
-                class C2 {
-                    fun apply(receiver: ImplicitReceiverClass): String =
-                        "--" + receiver.receiverString
-                }
-                
-                C2().apply(ImplicitReceiverClass(C1().y))
-            """.trimIndent()
-        ) {
-            implicitReceivers(ImplicitReceiverClass("Not Ok."))
-        }
-
-        assertTrue(
-            res.safeAs<ResultWithDiagnostics.Success<EvaluationResult>>()?.value?.returnValue?.safeAs<ResultValue.Value>()?.value == "--Ok.",
-            "test failed:\n  ${res.render()}"
-        )
-    }
-
-    @Test
     fun testScriptWithBoth() {
         val res = evalString<ScriptWithBoth>("""println(providedString + receiverString)""") {
             providedProperties("providedString" to "Hello")
@@ -81,6 +48,7 @@ class ConstructorArgumentsOrderTest {
         )
     }
 
-    private fun ResultWithDiagnostics<EvaluationResult>.render() =
-        reports.joinToString("\n  ") { it.message + if (it.exception == null) "" else ": ${it.exception!!.printStackTrace()}" }
 }
+
+internal fun ResultWithDiagnostics<EvaluationResult>.render() =
+    reports.joinToString("\n  ") { it.message + if (it.exception == null) "" else ": ${it.exception!!.printStackTrace()}" }
