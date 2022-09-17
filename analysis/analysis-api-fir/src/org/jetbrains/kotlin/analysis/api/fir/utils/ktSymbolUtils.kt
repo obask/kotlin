e1f00b4e48e821aea82ccf87d5bc773f7e45c635
Jaebaek Seo <jaebaek@google.com>
2022-09-17 02:26:25 +0000

    K2: add KtType.isArrayOrPrimitiveArray/isNestedArray()

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/utils/ktSymbolUtils.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/utils/ktSymbolUtils.kt
@@ -6,7 +6,9 @@
 package org.jetbrains.kotlin.analysis.api.fir.utils
 
 import org.jetbrains.kotlin.analysis.api.fir.symbols.KtFirSymbol
+import org.jetbrains.kotlin.analysis.api.fir.types.KtFirType
 import org.jetbrains.kotlin.analysis.api.symbols.KtSymbol
+import org.jetbrains.kotlin.analysis.api.types.KtType
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.LLFirResolveSession
 import org.jetbrains.kotlin.analysis.low.level.api.fir.project.structure.LLFirModuleData
 import org.jetbrains.kotlin.analysis.low.level.api.fir.project.structure.llFirModuleData
@@ -15,6 +17,8 @@ import org.jetbrains.kotlin.fir.dispatchReceiverClassOrNull
 import org.jetbrains.kotlin.fir.resolve.toFirRegularClassSymbol
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
+import org.jetbrains.kotlin.fir.types.arrayElementType
+import org.jetbrains.kotlin.fir.types.isArrayOrPrimitiveArray
 
 val KtSymbol.firSymbol: FirBasedSymbol<*>
     get() {
@@ -22,6 +26,19 @@ val KtSymbol.firSymbol: FirBasedSymbol<*>
         return this.firSymbol
     }
 
+/**
+ * Returns whether the given KtType is an array or a primitive array type or not.
+ */
+fun KtType.isArrayOrPrimitiveArray(): Boolean {
+    val firType = this as? KtFirType ?: return false
+    return firType.coneType.isArrayOrPrimitiveArray
+}
+
+fun KtType.isNestedArray(): Boolean {
+    if (!isArrayOrPrimitiveArray()) return false
+    val firType = this as? KtFirType ?: return false
+    return firType.coneType.arrayElementType()?.isArrayOrPrimitiveArray == true
+}
 
 fun FirBasedSymbol<*>.getContainingKtModule(firResolveSession: LLFirResolveSession): KtModule {
     val target = when (this) {
